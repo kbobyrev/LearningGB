a72ff48ba18c1a93250cc7a8ae263520f33167e3
kbobyrev <61451444+kbobyrev@users.noreply.github.com>
2020-08-23 22:51:43 +0300

    Create index.php

---
+++ lesson2/index.php
@@ -0,0 +1,22 @@
+<html>
+	<body>
+
+
+	<?php
+		//1
+		$a=-12;
+		$b=45;
+
+		if ($a>0 && $b>0){
+			echo $a-$b;	
+		}
+		elseif ($a<0 && $b<0) {
+			echo $a*$b;
+		}
+		else  {
+			echo $a+$b;
+		}
+
+	?>
+	</body>
+</html>
